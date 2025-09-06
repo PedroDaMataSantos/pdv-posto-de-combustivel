3a723965785074007d2af920a5e704611081cc86
PedroDaMataSantos <pescariaurbana@gmail.com>
2025-09-05 21:47:00 -0300

    New

--- PdvPostoCombustivel/src/main/java/com/br/domain/entity/Acesso.java
+++ PdvPostoCombustivel/src/main/java/com/br/domain/entity/Acesso.java
@@ -1,5 +1,40 @@
 package com.br.domain.entity;
 
 public class Acesso {
-    
+
+    //atributos
+
+    private String usuario;
+    private String senha;
+
+    //construtor 
+
+    public Acesso(String usuario,String semha){
+        this.usuario=usuario;
+        this.senha=semha;
+    }
+
+    //getters
+
+    public String getUsuario(){
+        return usuario;
+
+    }
+    public String senha(){
+        return senha;
+
+    }
+
+    //setters
+
+    public void setUsuario(String usuario){
+        this.usuario=usuario;
+    }
+    public void setSenha(String senha){
+        this.senha=senha;
+    }
+
+
+
+
 }
