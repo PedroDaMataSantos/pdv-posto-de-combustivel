ebdba1ca281f991867e6e525a2be1a342b19c44b
PedroDaMataSantos <pescariaurbana@gmail.com>
2025-09-05 21:03:35 -0300

    New Commit

---
+++ PdvPostoCombustivel/src/main/java/com/br/domain/entity/Pessoa.java
@@ -0,0 +1,60 @@
+package com.br.domain.entity;
+
+public class Pessoa {
+
+    // atributos
+    private String nomeCompleto;
+
+    private String cpfCnpj;
+
+    private Date dataNascimento;
+
+    private Integer numeroCtps;
+
+    //construtor
+    public Pessoa(String nomeCompleto, String cpfCnpj, Date dataNascimento, Integer numerocCtps) {
+        this.nomeCompleto = nomeCompleto;
+        this.cpfCnpj = cpfCnpj;
+        this.dataNascimento = dataNascimento;
+        this.numeroCtps = numeroCtps;
+
+    }
+
+    //getters
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public Integer getNumeroCtps() {
+        return numeroCtps;
+    }
+
+    //setter
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.numeroCtps = numeroCtps;
+    }
+
+}
+
+
